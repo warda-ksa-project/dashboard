c1cd8e3f0e3f30c420ce2274342156258d73d607
ahmedaladdin6 <ahmedaladdin6@gmail.com>
2025-02-21 20:30:34 +0200

    working on order

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -64,6 +64,8 @@ import { CategoryTableComponent } from './pages/category/category-table/category
 import { CategoryDetailsComponent } from './pages/category/category-details/category-details.component';
 import { SubCategoryTableComponent } from './pages/sub-category/sub-category-table/sub-category-table.component';
 import { SubCategoryDetailsComponent } from './pages/sub-category/sub-category-details/sub-category-details.component';
+import { ProductsTableComponent } from './pages/products/products-table/products-table.component';
+import { ProductsDetailsComponent } from './pages/products/products-details/products-details.component';
 
 export const routes: Routes = [
   {
@@ -154,6 +156,11 @@ export const routes: Routes = [
       { path: 'sub-category/add', component: SubCategoryDetailsComponent },
       { path: 'sub-category/view/:id', component: SubCategoryDetailsComponent },
       { path: 'sub-category/edit/:id', component: SubCategoryDetailsComponent },
+
+      { path: 'product', component: ProductsTableComponent },
+      { path: 'product/add', component: ProductsDetailsComponent },
+      { path: 'product/view/:id', component: ProductsDetailsComponent },
+      { path: 'product/edit/:id', component: ProductsDetailsComponent },
 
       { path: 'clients', component: ClientTableComponent },
       { path: 'client/add', component: ClientDetailsComponent },
---
+++ src/app/pages/products/products-details/products-details.component.ts
@@ -0,0 +1,225 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ButtonModule } from 'primeng/button';
+import { ApiService } from '../../../services/api.service';
+import { ActivatedRoute, Router, RouterModule } from '@angular/router';
+import { NgIf, TitleCasePipe } from '@angular/common';
+import { InputTextComponent } from '../../../components/input-text/input-text.component';
+import { BreadcrumpComponent } from "../../../components/breadcrump/breadcrump.component";
+import { IBreadcrumb } from '../../../components/breadcrump/cerqel-breadcrumb.interface';
+import { ConfirmMsgService } from '../../../services/confirm-msg.service';
+import { DialogComponent } from '../../../components/dialog/dialog.component';
+import { UploadFileComponent } from "../../../components/upload-file/upload-file.component";
+import { TranslatePipe } from '@ngx-translate/core';
+import { LanguageService } from '../../../services/language.service';
+import { IEditImage } from '../../../components/edit-mode-image/editImage.interface';
+import { EditModeImageComponent } from '../../../components/edit-mode-image/edit-mode-image.component';
+import { SelectComponent } from '../../../components/select/select.component';
+import { EditorComponent } from '../../../components/editor/editor.component';
+import { CheckBoxComponent } from '../../../components/check-box/check-box.component';
+
+const global_PageName='products.pageName';
+const global_routeUrl ='product'
+const global_API_details='product'+'/GetById';
+const global_API_create='product'+'/Create';
+const global_API_update='product'+'/Update';
+
+@Component({
+  selector: 'app-products-details',
+  standalone: true,
+  imports: [ReactiveFormsModule,CheckBoxComponent,SelectComponent,EditorComponent,EditModeImageComponent,TitleCasePipe,TranslatePipe, ButtonModule, NgIf, DialogComponent, InputTextComponent, RouterModule, BreadcrumpComponent, UploadFileComponent],
+  templateUrl: './products-details.component.html',
+  styleUrl: './products-details.component.scss'
+})
+export class ProductsDetailsComponent {
+
+
+  pageName =signal<string>(global_PageName);
+  private ApiService = inject(ApiService)
+  private router = inject(Router)
+  private route = inject(ActivatedRoute)
+  showConfirmMessage: boolean = false
+  private confirm = inject(ConfirmMsgService)
+  categoryList:any[]=[]
+  hasDiscount=false
+  
+  form = new FormGroup({
+    enName: new FormControl('', {
+      validators: [
+        Validators.required
+      ],
+    }),
+    arName: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    enDescription: new FormControl('', {
+      validators: [
+        Validators.required
+      ],
+    }),
+    arDescription: new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    stockQuantity:new FormControl('', {
+      validators: [
+        Validators.required
+      ]
+    }),
+    hasDiscount:new FormControl(),
+    discountType: new FormControl(),
+     amount: new FormControl(),
+    image: new FormControl(''),
+    id:new FormControl(this.getID|0),
+    categoryId:new FormControl()
+  })
+
+  bredCrumb: IBreadcrumb = {
+    crumbs: [ ]
+  }
+
+  editImageProps: IEditImage = {
+    props: {
+      visible: true,
+      imgSrc: ''
+    },
+    onEditBtn: (e?: Event) => {
+      this.editImageProps.props.visible = false;
+      this.editMode = false;
+    }
+  };
+
+  editMode: boolean = false;
+
+  get getID() {
+    return this.route.snapshot.params['id']
+  }
+  
+    selectedLang: any;
+    languageService = inject(LanguageService);
+
+  ngOnInit() {
+   
+    this.pageName.set(global_PageName)
+    this.getBreadCrumb();
+    this.getMainCategory()
+    this.languageService.translationService.onLangChange.subscribe(() => {
+      this.selectedLang = this.languageService.translationService.currentLang;
+      this.getBreadCrumb();
+      this.getMainCategory()
+    });
+    if (this.tyepMode() !== 'Add')
+      this.API_getItemDetails()
+
+    this.form.get('hasDiscount')?.valueChanges.subscribe((value: any) => {
+      this.form.get('discountType')?.reset();
+      this.form.get('amount')?.reset();
+
+      if (value) {
+        this.hasDiscount=true
+        this.form.get('discountType')?.setValidators([Validators.required]);
+        this.form.get('amount')?.setValidators([Validators.required]);
+      } else {
+        this.hasDiscount=false
+        this.form.get('discountType')?.clearValidators();
+        this.form.get('amount')?.clearValidators();
+      }
+      this.form.get('discountType')?.updateValueAndValidity();
+      this.form.get('amount')?.updateValueAndValidity();
+
+    });
+  }
+
+  tyepMode() {
+    const url = this.router.url;
+    let result='Add'
+    if (url.includes('edit')) result='Edit'
+    else if (url.includes('view')) result= 'View'
+    else result= 'Add'
+    return result
+  }
+
+  getBreadCrumb() {
+    this.bredCrumb = {
+      crumbs: [
+        {
+          label:  this.languageService.translate('Home'),
+          routerLink: '/dashboard',
+        },
+        {
+          label: this.languageService.translate(this.pageName()+ '_'+this.tyepMode()+'_crumb'),
+        },
+      ]
+    }
+  }
+
+  getMainCategory(){
+    this.ApiService.get('MainCategory/GetAll').subscribe((res: any) => {
+      if (res){
+        this.categoryList=[]
+        res.map((item:any) => {
+          this.categoryList.push({
+            name:this.selectedLang=='en'?item.enName:item.arName,
+            code:item.id
+          })
+        })
+      }
+       
+    })
+  }
+  API_getItemDetails() {
+    this.ApiService.get(`${global_API_details}`,{SubCategoryId:this.getID}).subscribe((res: any) => {
+      if (res)
+        this.form.patchValue(res)
+    })
+  }
+
+  onSubmit() {
+    const payload = {
+      ...this.form.value,
+    }
+    if (this.tyepMode() == 'Add')
+      this.API_forAddItem(payload)
+    else
+      this.API_forEditItem(payload)
+  }
+
+  navigateToPageTable(){
+    this.router.navigateByUrl(global_routeUrl)
+  }
+
+  cancel() {
+    const hasValue = this.confirm.formHasValue(this.form)
+    if (hasValue && this.tyepMode()=='Edit')
+      this.showConfirmMessage = !this.showConfirmMessage
+    else
+      this.navigateToPageTable()
+
+  }
+
+  onConfirmMessage() {
+    this.navigateToPageTable()
+
+  }
+
+
+  API_forAddItem(payload: any) {
+    this.ApiService.post(global_API_create, payload, { showAlert: true, message: `Add ${this.pageName()} Successfuly` }).subscribe(res => {
+      if (res)
+        this.navigateToPageTable()
+    })
+  }
+
+  API_forEditItem(payload: any) {
+    this.ApiService.put(global_API_update, payload, { showAlert: true, message: `update ${this.pageName()} Successfuly` }).subscribe(res => {
+      if (res)
+        this.navigateToPageTable()
+    })
+  }
+
+
+}
+
