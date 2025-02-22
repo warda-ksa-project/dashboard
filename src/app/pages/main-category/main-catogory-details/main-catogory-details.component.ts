0eef17ca8d69de1c5d566eac0e40e5c64021a479
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2025-02-23 01:02:35 +0200

    main category module

---
+++ src/app/pages/main-category/main-catogory-details/main-catogory-details.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-catogory-details',
+  standalone: true,
+  imports: [],
+  templateUrl: './main-catogory-details.component.html',
+  styleUrl: './main-catogory-details.component.scss'
+})
+export class MainCatogoryDetailsComponent {
+
+}
