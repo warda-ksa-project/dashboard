abb9f45f0291d07a7dc23e291c55977d1f0e29b2
aalaa <aalaa@youxel>
2025-04-18 20:31:47 +0200

    working on url

--- src/app/core/basic-auth.interceptor.ts
+++ src/app/core/basic-auth.interceptor.ts
@@ -9,19 +9,12 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
       setHeaders: {
         Authorization: `bearer ${localStorage.getItem('token')}`,
         'Accept-Language': localStorage.getItem('lang') === 'ar' ? 'ar' : 'en',
-        'Access-Control-Allow-Headers': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Credentials': 'true',
       }
     });
   }else{
     authReq = req.clone({
       setHeaders: {
-
         'Accept-Language': localStorage.getItem('lang') === 'ar' ? 'ar' : 'en',
-        'Access-Control-Allow-Headers': '*',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-        'Access-Control-Allow-Credentials': 'true',
       }
     });
   }
