925ed39bd5b8f771ebc9d0395aaee1368b3f68c6
aalaa <aalaa@youxel>
2025-04-18 19:37:27 +0200

    change URL

--- src/app/core/basic-auth.interceptor.ts
+++ src/app/core/basic-auth.interceptor.ts
@@ -8,7 +8,20 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     authReq = req.clone({
       setHeaders: {
         Authorization: `bearer ${localStorage.getItem('token')}`,
-        'Accept-Language': localStorage.getItem('lang') === 'ar' ? 'ar' : 'en'
+        'Accept-Language': localStorage.getItem('lang') === 'ar' ? 'ar' : 'en',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Credentials': 'true',
+      }
+    });
+  }else{
+    authReq = req.clone({
+      setHeaders: {
+
+        'Accept-Language': localStorage.getItem('lang') === 'ar' ? 'ar' : 'en',
+        'Access-Control-Allow-Headers': '*',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+        'Access-Control-Allow-Credentials': 'true',
       }
     });
   }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    // baseUrl: 'https://aspdev.matlop.com/api/',
-    baseUrl: "https://wardaweb-001-site1.qtempurl.com/api/",
-    //  baseImageUrl: 'https://aspdev.matlop.com',
-    baseImageUrl: "https://wardaweb-001-site1.qtempurl.com"
+    // baseUrl: "https://wardaweb-001-site1.qtempurl.com/api/",
+    baseUrl: "https://wrdah.com:6051/api/",
+    // baseImageUrl: "https://wardaweb-001-site1.qtempurl.com"
+    baseImageUrl: "https://wrdah.com:6051"
   };
