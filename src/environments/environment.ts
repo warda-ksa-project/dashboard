e8d2591638890f9fe453f12785a30cbd9870f147
aalaa <aalaa@youxel>
2025-04-18 21:11:10 +0200

    try old url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
-    baseUrl: "https://wrdah.com:6051/api/",
-    // baseUrl: "https://wardaweb-001-site1.qtempurl.com/api/",
-    baseImageUrl: "https://wrdah.com:6051"
-    // baseImageUrl:'https://wardaweb-001-site1.qtempurl.com/'
+    // baseUrl: "https://wrdah.com:6051/api/",
+    baseUrl: "https://wardaweb-001-site1.qtempurl.com/api/",
+    // baseImageUrl: "https://wrdah.com:6051"
+    baseImageUrl:'https://wardaweb-001-site1.qtempurl.com/'
   };
