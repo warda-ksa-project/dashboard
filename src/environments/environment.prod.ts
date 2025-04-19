e6a64ed6a118a25d12ab8a36f74fe6a291e2faa9
Ahmed Essam <40560225+iRashwan@users.noreply.github.com>
2025-04-19 12:16:49 +0200

    change base url on prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    baseUrl: "https://wardaweb-001-site1.qtempurl.com/api/",
-    // baseUrl: "https://wrdah.com:6051/api/",
-    baseImageUrl: "https://wardaweb-001-site1.qtempurl.com"
-    // baseImageUrl: "https://wrdah.com:6051"
+    // baseUrl: "https://wardaweb-001-site1.qtempurl.com/api/",
+    baseUrl: "https://wrdah.com:6051/api/",
+    // baseImageUrl: "https://wardaweb-001-site1.qtempurl.com"
+    baseImageUrl: "https://wrdah.com:6051"
   };
