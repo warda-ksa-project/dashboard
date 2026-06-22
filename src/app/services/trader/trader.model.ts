8857bba7bf0b9edd0626a99cec78474ea1716c48
M-E-Taha <elsha3rawy.btm@gmail.com>
2026-06-22 12:18:48 +0300

    Trader service && models

---
+++ src/app/services/trader/trader.model.ts
@@ -0,0 +1,60 @@
+export interface TraderAddress {
+  id: number;
+  street: string;
+  expalinedAddress: string;
+  cityId: number;
+  latitude: number;
+  longitude: number;
+}
+
+export interface UpdateProfileReqBody {
+  id: number;
+  name: string;
+  email: string;
+  phone: string;
+  phoneCountryCode: string;
+  storeName: string;
+  cr: string;
+  license: string;
+  iban: string;
+  numberOfBranches: number;
+  image: string;
+  arDescription: string;
+  enDescription: string;
+  addresses: [
+    {
+      id: number;
+      expalinedAddress: string;
+      cityId: number;
+      latitude: number;
+      logitude: number;
+    },
+  ];
+  supportsPickup: boolean;
+}
+
+export interface TraderProfile {
+  id: number;
+  userName: string;
+  email: string;
+  phone: string;
+  storeName: string;
+  cr: string;
+  license: string;
+  iban: string;
+  numberOfBranches: number;
+  isActive: boolean;
+  reviewAverage: number;
+  totalReviewCount: number;
+  image: number;
+  descriptionAr: string;
+  descriptionEn: string;
+  supportsPickup: boolean;
+  isAvailable: boolean;
+  addresses: TraderAddress[];
+  socialMedia: [];
+
+  phoneCountryCode: 'string';
+  arDescription: 'string';
+  enDescription: 'string';
+}
